10949fad3bd1b3a926d703a819de46468ecd2bff
Sonu Paswan <sonupas2806@gmail.com>
2021-09-11 19:13:24 +0530

    added average java file

---
+++ Average.java
@@ -0,0 +1,9 @@
+public class Average {
+    public static void main(String[] args) {
+        int a=90;
+        int b=98;
+        int c=67;
+        double average = (double)(a+b+c)/3;
+        System.out.println(average);
+    }
+}
