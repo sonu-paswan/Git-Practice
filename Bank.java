4f53bf87984940057073e91cca9ecb62113ef574
Sonu Paswan <sonupas2806@gmail.com>
2021-11-12 10:00:45 +0530

    mdf first

--- Bank.java
+++ Bank.java
@@ -1,4 +1,5 @@
 package BANK;
+import java.util.Scanner;
 class Bank{
     Bank(){};
     long accountNumber;
@@ -15,10 +16,10 @@ class AccountHolder extends Bank{ //account holder class
     String Occupation;
     
 
-    AccountHolder(long x, String IFSC, String name) { // constructor
-        this.accountNumber = x;
-        this.name = name;
+    AccountHolder(long accountNumber, String IFSC, String name) { // constructor
+        this.accountNumber = accountNumber;
         this.IFSC = IFSC;
+        this.name = name;
     }
 
     public void EnterMoredetail(String address, long cont, double b, String O, String Services) {
@@ -45,16 +46,30 @@ class AccountHolder extends Bank{ //account holder class
 
 class Main { //main class
     public static void main(String args[]) {
-        AccountHolder Sunny = new AccountHolder(33423123, "IBIBO902", "Sunny");
-        Sunny.EnterMoredetail("kandivali,Mumbai", 235579534, 55000, "teacher", "no locker");
-        AccountHolder Ramu = new AccountHolder(235546, "BARBONA123", "Ramu");
-        Ramu.EnterMoredetail("Nallasopara,Mumbai", 998343536, 2000000, "Engineer", "locker");
+        long Ac,contact;
+        double balance;
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter account number");
+        Ac=input.nextLong();
+        String Name,IF,Address,Ocup,Locker;
+        System.out.println("enter IFSC");
+        IF=input.nextLine();
+        System.out.println("enter name");
+        Name=input.nextLine();
+        System.out.println("enter address");
+        Address=input.nextLine();
+        System.out.println("enter contact");
+        contact=input.nextLong();
+        System.out.println("enter balance");
+        balance=input.nextDouble();
+        System.out.println("enter occuption and locker system");
+        Ocup=input.nextLine();
+        Locker=input.nextLine();
+        AccountHolder obj1 = new AccountHolder(Ac,IF , Name);
+        obj1.EnterMoredetail(Address,contact, balance, Ocup, Locker);
 
-        double loan = Sunny.CheckLoan();
+        double loan = obj1.CheckLoan();
         System.out.println(loan);
-
-        double x = Ramu.CheckLoan();
-        System.out.println(x);
     }
 
 }
